80505fca6d552a202566afeb855481d080e25755
Bastien Hirsou <bastien.hirsou@adeo.com>
2026-08-17 16:58:50 +0200

    extra deck

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SHELL = "poolretro-shell-v8";
+const SHELL = "poolretro-shell-v10";
 const ART = "poolretro-art-v1";
 const FILES = ["./", "./index.html", "./manifest.json",
   "./apple-touch-icon.png", "./icon-192.png", "./icon-512.png"];
